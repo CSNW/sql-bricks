CSNW/sql-bricks

commit 25f46ca68a3a4e028a042f2870d86f1be7333a0c

Write findIndex with a for loop

sql-bricks.js
@@ -58,14 +58,12 @@
   };
 
   function findIndex(arr, fn) {
-    var arr_index = -1;
-    arr.forEach(function(val, index) {
+    for (var i = 0; i < arr.length; i++) {
       if (fn(val)) {
-        arr_index = index;
-        return;
+        return i;
       }
-    });
-    return index;
+    }
+    return -1;
   }
 
   function isEmpty(obj) {

commit 3f544f1d9c5e5dae431e29c32b0df7e99abcf795

Fixed findIndex bug introduced in 25f46ca68a, added regression test

sql-bricks.js
@@ -59,7 +59,7 @@
 
   function findIndex(arr, fn) {
     for (var i = 0; i < arr.length; i++) {
-      if (fn(val)) {
+      if (fn(arr[i])) {
         return i;
       }
     }

tests/tests.js
@@ -974,6 +974,23 @@ describe('SQL Bricks', function() {
       assert(ext.select.prototype.clauses !== sql.select.prototype.clauses)
     });
   });
+
+  describe('defineClause', function() {
+    // this extension mechanism is used by sql-bricks-postgres
+    it('should be able to inject clauses after other clauses', function() {
+      var Insert = sql.insert;
+      Insert.defineClause('returning', function(opts) {
+        return `RETURNING test_column`;
+      }, {after: 'values'});
+
+      check(insert('user', {'id': 33, 'name': 'Fred'}),
+        "INSERT INTO \"user\" (id, name) VALUES (33, 'Fred') RETURNING test_column");
+
+      // remove the newly-added clause, so subsequent INSERT tests won't fail
+      var ix = Insert.prototype.clauses.findIndex(clause => clause.clause_id == 'returning');
+      Insert.prototype.clauses.splice(ix, 1);
+    });
+  })
 });
 
 function check(stmt, expected) {